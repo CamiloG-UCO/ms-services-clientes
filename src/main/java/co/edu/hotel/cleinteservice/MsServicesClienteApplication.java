e897c724f6ca017236ade34b12ebf25797c7712d
Camilo Garcia <juan092005ca@gmail.com>
2025-10-16 20:10:10 -0500

    First commit

---
+++ src/main/java/co/edu/hotel/cleinteservice/MsServicesClienteApplication.java
@@ -0,0 +1,13 @@
+package co.edu.hotel.cleinteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServicesClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServicesClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/hotel/cleinteservice/MsServicesClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.hotel.cleinteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServicesClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
